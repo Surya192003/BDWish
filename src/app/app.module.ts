0d7278d318e48ca9e7659b87773b234e902147cf
Surya192003 <suryasonofsubbu@gmail.com>
2024-12-10 12:18:20 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './WISH_COMPONENTS/home-page/home-page.component';
 import { HapppyBirthdayComponent } from './WISH_COMPONENTS/happpy-birthday/happpy-birthday.component';
+import { SlideShowComponent } from './WISH_COMPONENTS/slide-show/slide-show.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    HapppyBirthdayComponent
+    HapppyBirthdayComponent,
+    SlideShowComponent
   ],
   imports: [
     BrowserModule,
